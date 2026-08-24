c68bd99e55218016b3eeeea9c3ad64e3ac58d6af
hac52 <marcel.woelfle@outlook.de>
2026-08-24 04:24:09 +0200

    Update SwitchBotUsermod.cpp

--- usermods/SwitchBot/SwitchBotUsermod.cpp
+++ usermods/SwitchBot/SwitchBotUsermod.cpp
@@ -1,5 +1,5 @@
-#include "SwitchBotUsermod.h"
+#include "SwitchBot_PM1_Cloud.h"
 
 // Registriert den Usermod beim WLED-System, damit er mitkompiliert wird
-static SwitchBotPM1Cloud switchbot_pm1_usermod;
-REGISTER_USERMOD(switchbot_pm1_usermod);
+static SwitchBotP switchbot_usermod;
+REGISTER_USERMOD(switchbot_usermod);
