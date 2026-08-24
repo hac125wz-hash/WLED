28d2cf4168ebe040b4870783dbfef47d6fe59802
hac52 <marcel.woelfle@outlook.de>
2026-08-24 02:53:27 +0200

    Add files via upload

---
+++ usermods/SwitchBot/SwitchBotUsermod.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "wled.h"
+
+class SwitchBotUsermod : public Usermod {
+private:
+  bool enabled = true;
+  String apiToken = "";
+  String deviceId = "";
+  uint32_t pollInterval = 60000; // ms
+  uint32_t lastMillis = 0;
+  String lastStatus = "unknown";
+
+  void fetchDeviceStatus();
+
+public:
+  SwitchBotUsermod();
+  void setup() override;
+  void loop() override;
+  void addToJsonInfo(JsonObject &root) override;
+  void readFromConfig(JsonObject &root) override;
+  void addToConfig(JsonObject &root) override;
+};
