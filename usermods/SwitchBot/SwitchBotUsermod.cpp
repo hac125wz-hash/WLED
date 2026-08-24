9cc25ed0f2b083a5403ae1e876907a8f5bb6dc04
hac125wz-hash <hac125wz@gmail.com>
2026-08-24 16:30:14 +0200

    Delete usermods/SwitchBot directory

--- usermods/SwitchBot/SwitchBotUsermod.cpp
+++
@@ -1,5 +0,0 @@
-#include "SwitchBot_PM1_Cloud.h"
-
-// Registriert den Usermod beim WLED-System, damit er mitkompiliert wird
-static SwitchBotPM1Cloud switchbot_usermod;  // Changed: SwitchBot -> SwitchBotPM1Cloud
-REGISTER_USERMOD(switchbot_usermod);
